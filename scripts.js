d110e0b42b545b27ffec35db700ba74453afe832
Daniel <danielmartins10@gmail.com>
2024-08-20 16:08:24 -0300

    feat: comment li and update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 // Seleciona os elementos do formulário
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
 
 // Captura o evento de input para formatar o valor
 amount.oninput = () => {
@@ -20,6 +23,37 @@ function formatCurrencyBRL(value) {
         currency: "BRL"
     })
 
-    // Retorna o valor formatado
+    // Retorna o valor formatado 
     return value
+}
+
+// Captura o evento de submit do formulário para obter os valores 
+form.onsubmit = (event) => {
+    // Previne o comportamento padrão de recarregar a página
+    event.preventDefault()
+
+    // Cria um objeto com os detalhes da nova despesa
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        created_at: new Date(),
+    }
+
+    // Chama a função que irá adicionar o item na lista
+    expenseAdd(newExpense)
+}
+
+function expenseAdd(newExpense) {
+    try {
+        // Cria o elemento para a adicionar o item (li) na lista (ul)
+        const expenseItem = document.createElement("li")
+        expenseItem.classList.add("expense")
+
+    } catch (error) {
+        alert("Não foi possível atualizar a lista de despesas.")
+        console.log(error)
+    }
 }
